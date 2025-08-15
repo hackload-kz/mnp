86a578449bcc1c8c888e4f311ab1cf079858b1ea
Batir Kenjaev <batya.kenjaev@gmail.com>
2025-08-15 11:27:37 +0500

    init

---
+++ biletter/src/main/java/mn/partners/biletter/BiletterApplication.java
@@ -0,0 +1,13 @@
+package mn.partners.biletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiletterApplication.class, args);
+	}
+
+}
---
+++ biletter/src/test/java/mn/partners/biletter/BiletterApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.partners.biletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
