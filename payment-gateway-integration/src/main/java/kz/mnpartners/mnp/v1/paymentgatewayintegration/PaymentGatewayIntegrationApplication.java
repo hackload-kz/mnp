12e23f5354a8e46aefb17c73fcd5b006929bd65e
Ermagombet Darkhan <darhanmustafin@mail.ru>
2025-08-16 10:17:01 +0500

    init payment integration

---
+++ payment-gateway-integration/src/main/java/kz/mnpartners/mnp/v1/paymentgatewayintegration/PaymentGatewayIntegrationApplication.java
@@ -0,0 +1,13 @@
+package kz.mnpartners.mnp.v1.paymentgatewayintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentGatewayIntegrationApplication.class, args);
+    }
+
+}
